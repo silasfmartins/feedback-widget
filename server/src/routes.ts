ae7b160248eabfee3eb3195aacb3f3946a3469ce
Silas Martins <silas.martins2041@gmail.com>
2022-05-14 14:40:03 -0300

    Tirando envio de e-mails

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository"
-import { NodemailerMailAdapter } from './adapters/nodemailer/node-mail-adapter';
 
 export const routes = express.Router()
 
@@ -9,10 +8,8 @@ routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
   try {
     const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
     const submitFeedbackUseCase = new SubmitFeedbackUseCase(
       prismaFeedbacksRepository,
-      nodemailerMailAdapter
     )
     await submitFeedbackUseCase.execute({
       type,
