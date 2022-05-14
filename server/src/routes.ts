845aaf1f9e69ec0f2c86a8e844fbb56a086dc634
Silas Martins <silas.martins2041@gmail.com>
2022-05-14 11:52:16 -0300

    Test error

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -7,16 +7,21 @@ export const routes = express.Router()
 
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-  const nodemailerMailAdapter = new NodemailerMailAdapter()
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter
-  )
-  await submitFeedbackUseCase.execute({
-    type,
-    comment,
-    screenshot,
-  })
-  return res.status(200).send();
+  try {
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter
+    )
+    await submitFeedbackUseCase.execute({
+      type,
+      comment,
+      screenshot,
+    });
+  } catch(err) {
+    console.error(err);
+
+    return res.status(500).send();
+  }
 });
